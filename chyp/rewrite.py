fd7092c3f0fc3e325d0073451b2b362b1dcddab2
Aleks Kissinger <aleks0@gmail.com>
2022-09-09 09:10:33 +0100

    rewriter

--- chyp/rewrite.py
+++ chyp/rewrite.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Set, List, Dict, Iterator, Any, Optional, Iterable
 import itertools
 from .graph import Graph
+from .matcher import Match
 
 class Rule:
     lhs: Graph
@@ -18,3 +19,19 @@ class Rule:
             if v in verts: return False
             verts.add(v)
         return True
+
+def rewrite(g: Graph, r: Rule, m: Match) -> Graph:
+    if not r.is_left_linear():
+        raise NotImplementedError("Only left linear rules are supported for now")
+
+    h = g.copy()
+
+    # compute the pushout complement
+    for e in r.lhs.edges():
+        h.remove_edge(m.emap[e])
+    for v in r.lhs.vertices():
+        if not r.lhs.is_boundary(v):
+            h.remove_vertex(m.vmap[v])
+
+    return h
+
